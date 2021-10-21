810588d77a99319ec7d93ee2e55ca6556e414e8b
Henrique Kirch Heck <henriquekirchheck@gmail.com>
2021-10-21 14:20:53 -0300

    Finally found a way to access the .env file

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -2,6 +2,8 @@ import LoginBoxStyles from './styles.module.scss'
 import { VscGithubInverted } from "react-icons/vsc"
 
 function LoginBox() {
+  const signInURL = `https://github.com/login/oauth/authorize?scope=user&client_id=${import.meta.env.VITE_GITHUB_CLIENT_ID}`
+
   return (
     <div className={LoginBoxStyles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
